e6ccede4941dc9bff8dfd17950764729a79f6915
ithamarSpitz <itas12@gmail.com>
2024-11-26 11:36:30 +0200

    back body

--- myApp/src/components/BackButton.tsx
+++ myApp/src/components/BackButton.tsx
@@ -1,1 +1,12 @@
+import { IonBackButton } from '@ionic/react';
 import './BackButton.css';
+
+
+
+const BackButton: React.FC = () => {
+  return (
+    <IonBackButton />
+  );
+};
+
+export default BackButton;
