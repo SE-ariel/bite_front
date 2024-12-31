d6ac142400809565c551d6273634d906c093c1a0
shaharBabkoff <shahar.babkoff@msmail.ariel.ac.il>
2024-12-31 09:55:22 +0200

    take useeffect to different file

--- myApp/src/LoggedInRouter.tsx
+++ myApp/src/LoggedInRouter.tsx
@@ -1,9 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 import { IonApp, IonRouterOutlet } from "@ionic/react";
 import { IonReactRouter } from "@ionic/react-router";
-import { auth, db } from "./firebaseConfig";
-import { doc, getDoc } from "firebase/firestore";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -13,28 +11,10 @@ import Settings from "./pages/Settings";
 import NotFound from "./pages/NotFound";
 import Loading from "./pages/Loading";
 import SetUpProfile from "./pages/SetupProfile";
+import useUserProfileCheck from "./logics/useUserProfileCheck";
 
 const LoggedInRouter: React.FC = () => {
-  const [isUserChecked, setIsUserChecked] = useState(false);
-  const [needsSetup, setNeedsSetup] = useState(false);
-
-  useEffect(() => {
-    const checkUserProfile = async () => {
-      const user = auth.currentUser; 
-      if (user) {
-        const userRef = doc(db, "users", user.uid);
-        const userSnap = await getDoc(userRef);
-
-        
-        if (!userSnap.exists() || !userSnap.data().firstName) {
-          setNeedsSetup(true);
-        }
-      }
-      setIsUserChecked(true);
-    };
-
-    checkUserProfile();
-  }, []);
+  const { isUserChecked, needsSetup } = useUserProfileCheck();
 
   if (!isUserChecked) {
     return <Loading />;
@@ -43,7 +23,7 @@ const LoggedInRouter: React.FC = () => {
   return (
     <IonApp>
       <IonReactRouter>
-        {/* הפנייה אוטומטית לדף setup אם המשתמש צריך להשלים פרטים */}
+        {/* Redirect to setup page if user needs to complete profile */}
         {needsSetup && <Redirect to="/setup" />}
         
         <IonRouterOutlet>
@@ -52,7 +32,7 @@ const LoggedInRouter: React.FC = () => {
             <Route path="/setup" component={SetUpProfile} exact />
             <Route path="/login" component={Login} exact />
             <Route path="/register" component={Register} exact />
-             {/* Logged In Routes - Only render if logged in */}
+            {/* Logged In Routes - Only render if logged in */}
             <Route exact path="/home">
               <LoggedInFrame title="home" wrappedContent={Home} />
             </Route>
