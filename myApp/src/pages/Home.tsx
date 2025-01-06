bdb7b150e2660945496d3df422738fce68b5e74e
ithamarSpitz <itas12@gmail.com>
2025-01-06 13:17:01 +0200

    Refactor image handling: remove GetImage component, enhance UploadImage with loading state, and add ImageDisplay for rendering images from Firestore.

--- myApp/src/pages/Home.tsx
+++ myApp/src/pages/Home.tsx
@@ -2,14 +2,15 @@ import { IonContent } from "@ionic/react";
 import "./Home.css";
 import GoToProfile from "../components/GoToProfile";
 import UploadImage from "../components/UploadImage";
+import ImageDisplay from "../components/ImageDisplay";
 
 const Home: React.FC = () => {
   return (
     <IonContent fullscreen>
       <GoToProfile />
-      <UploadImage id="O6rVwgcwRw3gLdr0SZ5v" />
+      <UploadImage id="yYRqmvJzEmOsY6J9aMTt" />
+      <ImageDisplay documentId="yYRqmvJzEmOsY6J9aMTt" />
     </IonContent>
   );
 };
-
 export default Home;
