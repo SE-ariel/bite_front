8b4eabba7f3b37111e4d1b5ccb75fa404a3c0abc
ithamarSpitz <itas12@gmail.com>
2025-01-09 19:58:42 +0200

    Add CreatePostButton component and integrate it into Header; clean up LoggedInRouter formatting

--- myApp/src/components/Header.tsx
+++ myApp/src/components/Header.tsx
@@ -3,11 +3,15 @@ import BackButton from "./BackButton";
 import PrivateZoneButton from "./PrivateZoneButton";
 import HomeButton from "./HomeButton";
 import NotificationsButton from "./NotificationsButton";
+import CreatePostButton from "./CreatePostButton";
+import { useRoleStatus } from "../logics/Role";
 interface Props {
   title: string;
 }
 
 const Header: React.FC<Props> = (props) => {
+  const role = useRoleStatus();
+
   return (
     <IonHeader>
       <IonToolbar>
@@ -16,12 +20,11 @@ const Header: React.FC<Props> = (props) => {
           <IonTitle slot="end" size="large">
             {props.title}
           </IonTitle>
-          {props.title !== "private zone" && 
-            <PrivateZoneButton />}
-          {props.title !== "home" && 
-            <HomeButton />}          
-          {props.title !== "notifications" && 
-            <NotificationsButton />}
+          {props.title !== "private zone" && <PrivateZoneButton />}
+          {props.title !== "home" && <HomeButton />}
+          {props.title !== "notifications" && <NotificationsButton />}
+          {(role === "Admin" || role === "ContentCreator") &&
+            props.title !== "create post" && <CreatePostButton />}
         </IonButtons>
       </IonToolbar>
     </IonHeader>
